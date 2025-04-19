d648d8765fcde0e1afb42812a4774147a9bf9e60
Aziz <abdelaziz.aidel@gmail.com>
2025-04-20 00:12:30 +0200

    Ajout du test K6 dans le Jenkinsfile

--- cypress/e2e/test_jenkins.cy.js
+++ cypress/e2e/test_jenkins.cy.js
@@ -1,30 +1,11 @@
-describe("Mon premier test", () => {
-  it("Visite Google et tape une recherche", () => {
-    // Aller sur Google
-    cy.visit("https://www.google.com");
+describe("Test de démo sur example.cypress.io", () => {
+  it("Remplit un champ et vérifie le résultat", () => {
+    cy.visit("https://example.cypress.io/commands/actions");
 
-    // Attendre un peu que la page charge
-    cy.wait(2000);
+    // Tape du texte dans un champ
+    cy.get(".action-email").type("aziz@example.com");
 
-    // Gérer le consentement RGPD si visible
-    cy.get("body").then(($body) => {
-      if ($body.find('button:contains("J’accepte")').length > 0) {
-        cy.contains("J’accepte").click({ force: true });
-      } else if ($body.find('button:contains("Tout accepter")').length > 0) {
-        cy.contains("Tout accepter").click({ force: true });
-      }
-    });
-
-    // Attendre à nouveau un peu après consentement
-    cy.wait(2000);
-
-    // Trouver la barre de recherche
-    cy.get('input[name="q"]', { timeout: 10000 }).should("be.visible");
-
-    // Entrer une recherche
-    cy.get('input[name="q"]').type("Jenkins{enter}");
-
-    // Vérifier que les résultats apparaissent
-    cy.get("#search", { timeout: 10000 }).should("exist");
+    // Vérifie que le champ contient bien ce qu’on a tapé
+    cy.get(".action-email").should("have.value", "aziz@example.com");
   });
 });
